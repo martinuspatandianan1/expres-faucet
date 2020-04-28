344fa7b1689f2934fa3b28ceb639c32fae36e8c0
martinuspatandianan1 <63387920+martinuspatandianan1@users.noreply.github.com>
2020-04-28 23:17:35 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(0);
 
-$coki_doge = array("");
-$coki_ltc = array("");
-$coki_eth = array("");
+$coki_doge = array("cookie: DOGEToken=ChJhMJg1L9es2URGPAS2RvHi6kj0UB7s; PHPSESSID=7ef600434dd6a121a749a8e1b3651b1f; _ga=GA1.2.985634497.1588086354; _gid=GA1.2.1847187776.1588086354; _gat_gtag_UA_163156743_1=1");
+$coki_ltc = array("cookie: CreditsEmpty=1; LTCToken=PYFmblXoxpnNsP7rX6zftokWm4Lh2zuz; PHPSESSID=7ef600434dd6a121a749a8e1b3651b1f; _ga=GA1.2.985634497.1588086354; _gid=GA1.2.1847187776.1588086354; _gat_gtag_UA_163156743_1=1");
+$coki_eth = array("cookie: ETHToken=JcwPsplg3OqcJescaiLS0qQVRlv4daez; PHPSESSID=7ef600434dd6a121a749a8e1b3651b1f; _ga=GA1.2.985634497.1588086354; _gid=GA1.2.1847187776.1588086354; _gat_gtag_UA_163156743_1=1");
 $coki_dash = array("");
 $coki_xmr = array("");
 $coki_trx = array("");
